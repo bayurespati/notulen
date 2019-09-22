e51eafea51d646ee3838011a599ba86869963dac
bayurespati <respatibayu48@gmail.com>
2019-09-23 04:44:15 +0700

    modified table user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->bigInt('companies_id');
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('address');
+            $table->string('current_position');
+            $table->string('primary_contact');
+            $table->string('secondary_contact');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
